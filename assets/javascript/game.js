5e5f4dc741574b137d91c8898f3c19ff33811d3a
unknown <msvendsentan@gmail.com>
2018-06-29 18:24:58 -0400

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBO_Cds5R035iJkeym4BGQnhFI4ZUPLXAc",
+    authDomain: "utor-coding-bootcamp-rpsgame.firebaseapp.com",
+    databaseURL: "https://utor-coding-bootcamp-rpsgame.firebaseio.com",
+    projectId: "utor-coding-bootcamp-rpsgame",
+    storageBucket: "",
+    messagingSenderId: "690188252275"
+};
+firebase.initializeApp(config);
